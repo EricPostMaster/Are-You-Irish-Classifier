07957b00a31c1118216053ae49d80c8868a7d98e
Eric Sims <58200144+EricPostMaster@users.noreply.github.com>
2022-03-12 22:02:56 -0500

    Update ngram_creator docstring

--- main.py
+++ main.py
@@ -5,29 +5,26 @@ from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import ComplementNB
 
-# Unpickle the Pipeline object
+# Unpickle the Pipeline and log probabilities objects
 with open("saved-objects/pickled_CompNB_model.p", "rb") as p:
     model = pickle.load(p)
-
 with open("saved-objects/irish_log_probs.p", "rb") as p:
     feature_probs = pickle.load(p)
 
 
 def ngram_creator(term_list, n=4):
-    """
-    Takes two arguments: a list of words to break into ngrams and the length of the ngram
-
-    Inputs:
-        term_list: [list] terms to break into intraword ngrams (example: df['names'])
-        n: [int] desired length of ngrams
-    
-    Outputs:
-        ngrams of individual words: e.g., bigrams for 'dog' = ['do','og']
-        gram_string_list: list where each element is the set of n-grams for each original record. Use in CountVectorizer
+    """Return list of ngrams for given term(s)
+    Parameters
+    ----------
+    term_list: list of terms to break into ngrams (e.g., ['Murphy'])
+    n: int for the length of ngrams
+    Returns
+    -------
+    word_grams: list of ngrams of individual words: e.g., 4grams for 'Murphy' = [' mur','murp','urph','rphy','phy ']
     """
 
-    gram_string_list = []
-    gram_length = n
+    # gram_string_list = []
+    # gram_length = n
 
     for i in term_list:
         i = str(i)
@@ -36,16 +33,15 @@ def ngram_creator(term_list, n=4):
         i = i.lower()
         i = " "+i+" "  # Add initial and terminal clusters
 
-        # break
         word_grams = []
         gram_string = ""
-        for j in range(gram_length,100):
-            gram = i[j-gram_length:j]
-            if len(gram) == gram_length:  # only keep ngrams of the correct length
+        for j in range(n,100):
+            gram = i[j-n:j]
+            if len(gram) == n:  # only keep ngrams of the correct length
                 word_grams.append(gram)
                 gram_string = gram_string + gram + " "
-        gram_string = gram_string[:-1]  # Cut that last space off the end there
-        gram_string_list.append(gram_string)  # Append the ngrams for the current name as a space-separated string
+        # gram_string = gram_string[:-1]  # Cut that last space off the end there
+        # gram_string_list.append(gram_string)  # Append the ngrams for the current name as a space-separated string
     
     return word_grams #gram_string_list
 
@@ -57,8 +53,6 @@ st.set_page_config(
 	page_icon=":shamrock:",
 	layout="centered"
 )
-
-
 
 
 
@@ -78,18 +72,12 @@ had but always needed: **your Murphy Score**. It's a score that tells you how Ir
 name is relative to the most Irish of names: *Murphy*.""")
 st.write("""What are you waiting for? Type your surname below and hit Enter!""")
 
-
-
 user_name = [st.text_input('Your last name')]
 if user_name:
-    # Predict it with the model.
-    # st.write(model.predict_proba(user_name))
+    # Ngrams for the user's name input
     ngram_list = ngram_creator(user_name)
 
-    # st.write(feature_probs[0:2])
-    # st.write(list(filter(lambda prob: prob[0] == ' sim', feature_probs))[0][1])
-    # st.write(ngram_list[0])
-
+    # Compile probabilities from input name ngrams
     attempted_flag = 0
     ngrams_and_probs = []
     for ngram in ngram_list:
@@ -99,12 +87,10 @@ if user_name:
         except:
             attempted_flag = 1
             continue
-
-    # st.write(ngrams_and_probs)
-
+    
     user_name_log_probs = [p for _, p in ngrams_and_probs]
 
-    # st.write(user_name_log_probs)
+    # If a name has been entered and has at least 1 probability available from the model
     if len(ngrams_and_probs) > 0:
 
         user_name_avg_log_prob = sum(user_name_log_probs) / float(len(ngrams_and_probs))
@@ -113,17 +99,12 @@ if user_name:
 
         st.write(f"Your Murphy Score is: {murphy_score}/100!")
         st.write("That's definitely good enough for a drink! :shamrock: :beers:")
-    
+
+    # If something other than the initial empty name field has been entered but there were no ngram matches
     elif attempted_flag == 1:
         st.write("Well, Janey Mack, look at that! We've never seen a name like yours!")
         st.write("Even so, it'd be a pity to leave a jar lonely on St. Patty's Day, so drink up! :beers:")
     
+    # The page starts with this
     else:
         st.write(f"Your Murphy Score is: ___/100")
-    # Take each element of the user_name list and get the log probability from feature_probs
-
-    # Calculate the average log_probability of the absolute values of each probability
-
-
-    # If none are there, then return a statement like,
-        # "Well, Janey Mack, look at that! We've never seen a name like yours!"
